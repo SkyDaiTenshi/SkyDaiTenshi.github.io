82f9a574bcee07ae6dbac5de608e8460f66d034a
SkyDaiTenshi <holyone@abv.bg>
2019-12-04 16:05:09 +0200

    hi

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mainURL = 'https://api.giphy.com/v1/gifs/';
 const apiKey = `j99F6FK9wcELlAPJnktep9Hxk5Qc8b2s`;
 const limitSearch = '5';
 const getUrl = `${mainURL}trending?api_key=${apiKey}&limit=${limitSearch}&rating=G`;
-
+console.log("hi");
 fetch(getUrl)
 .then(response => {
     return response.json()
@@ -12,7 +12,6 @@ fetch(getUrl)
     let trendGrid = document.getElementById("trendGrid");
     console.log(gifUrls)
     gifUrls.forEach(element => {
-      console.log("in")
       let img = img = new Image();
       trendGrid.innerHTML += '<img src="'+element.url+'" />';
     });
