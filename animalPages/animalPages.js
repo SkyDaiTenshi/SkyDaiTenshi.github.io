65cc38256bff5ed3dee6df09af260e6d1b7444f5
SkyDaiTenshi <holyone@abv.bg>
2019-12-10 21:42:33 +0200

    changing index

---
+++ animalPages/animalPages.js
@@ -0,0 +1,38 @@
+const mainURL = 'https://api.giphy.com/v1/gifs/';
+const apiKey = `j99F6FK9wcELlAPJnktep9Hxk5Qc8b2s`;
+const limitSearch = '16';
+let keyword = document.getElementById("random").textContent;
+console.log('js works')
+const getUrl = `${mainURL}search?&api_key=${apiKey}&q=${keyword}&limit=${limitSearch}&offset=0&rating=G&lang=eng`;
+fetch(getUrl)
+.then(response => {
+    return response.json()
+  })
+  .then(data => {
+    let gifData = data.data;
+    let gifUrls=[];
+    let trendGrid = document.getElementById("trendGrid");
+ 
+    gifData.forEach(element => {
+      gifUrls.push(element.images.original.url)
+    });
+    console.log(gifUrls)
+    for (let i=0; i<gifUrls.length; i++){
+    trendGrid.innerHTML += '<img src="'+gifUrls[i]+' "class="gallery__img" />';
+    }
+
+  })
+  .then(
+1
+  )
+
+  .catch(err => {
+    console.log("Fetch Error");
+  })
+
+  document.getElementById("cats").addEventListener("click", catPage());
+
+  function catPage () {
+    console.log("test works")
+    // window.location = 'https://skydaitenshi.github.io/animalPages/cats.html';
+  }
