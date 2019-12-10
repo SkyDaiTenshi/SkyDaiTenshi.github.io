9ad9d8edc983126d1729094dd7e943b3f007b22d
SkyDaiTenshi <holyone@abv.bg>
2019-12-10 16:05:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mainURL = 'https://api.giphy.com/v1/gifs/';
 const apiKey = `j99F6FK9wcELlAPJnktep9Hxk5Qc8b2s`;
 const limitSearch = '16';
 let keyword = document.getElementById("zone").innerText;
+console.log(keyword);
 const getUrl = `${mainURL}search?&api_key=${apiKey}&q=$keyword&limit=${limitSearch}&offset=0&rating=G&lang=eng`;
 fetch(getUrl)
 .then(response => {
