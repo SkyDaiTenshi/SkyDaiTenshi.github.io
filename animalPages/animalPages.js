7f9a1c7c72baea7ffe9f385a0f58ca5143032ed9
SkyDaiTenshi <holyone@abv.bg>
2019-12-10 22:06:51 +0200

    event listeners

--- animalPages/animalPages.js
+++ animalPages/animalPages.js
@@ -2,7 +2,7 @@ const mainURL = 'https://api.giphy.com/v1/gifs/';
 const apiKey = `j99F6FK9wcELlAPJnktep9Hxk5Qc8b2s`;
 const limitSearch = '16';
 let keyword = document.getElementById("random").textContent;
-console.log('js works')
+
 const getUrl = `${mainURL}search?&api_key=${apiKey}&q=${keyword}&limit=${limitSearch}&offset=0&rating=G&lang=eng`;
 fetch(getUrl)
 .then(response => {
@@ -29,10 +29,3 @@ fetch(getUrl)
   .catch(err => {
     console.log("Fetch Error");
   })
-
-  document.getElementById("cats").addEventListener("click", catPage());
-
-  function catPage () {
-    console.log("test works")
-    // window.location = 'https://skydaitenshi.github.io/animalPages/cats.html';
-  }
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-document.getElementById("cats").addEventListener("click", catPage());
 
-function catPage () {
-  console.log("test works")
-  // window.location = 'https://skydaitenshi.github.io/animalPages/cats.html';
-}
+// function catPage () {
+//     console.log('js works')
+
+// //   window.location = 'https://skydaitenshi.github.io/animalPages/cats.html';
+// }
+// document.getElementById("cats").addEventListener("click", catPage());
+
+document.getElementById("cats").addEventListener("click", function(){
+    // document.getElementById("demo").innerHTML = "Hello World";
+    console.log('js works')
+    window.location = 'https://skydaitenshi.github.io/animalPages/cats.html';
+  }); 
