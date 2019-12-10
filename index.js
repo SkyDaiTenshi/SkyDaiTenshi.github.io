ff5208feec00248e1c082050029df0162f727f02
SkyDaiTenshi <holyone@abv.bg>
2019-12-10 17:29:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mainURL = 'https://api.giphy.com/v1/gifs/';
 const apiKey = `j99F6FK9wcELlAPJnktep9Hxk5Qc8b2s`;
 const limitSearch = '16';
-let keyword = document.getElementById("zone").textContent;
+console.log(document.getElementById("zone").textContent);
 
 
 const getUrl = `${mainURL}search?&api_key=${apiKey}&q=$keyword&limit=${limitSearch}&offset=0&rating=G&lang=eng`;
