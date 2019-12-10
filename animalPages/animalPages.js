a16ace1a0d2aaca2cbcc4ac63b26a0a4f674477c
SkyDaiTenshi <holyone@abv.bg>
2019-12-11 00:38:47 +0200

    almost done

--- animalPages/animalPages.js
+++ animalPages/animalPages.js
@@ -1,31 +1,53 @@
 const mainURL = 'https://api.giphy.com/v1/gifs/';
 const apiKey = `j99F6FK9wcELlAPJnktep9Hxk5Qc8b2s`;
 const limitSearch = '16';
-let keyword = (document.getElementById("random").textContent).split;
+let keyword = document.getElementById("zone").textContent.split(" ");
 
-const getUrl = `${mainURL}search?&api_key=${apiKey}&q=${keyword[1]}&limit=${limitSearch}&offset=0&rating=G&lang=eng`;
-fetch(getUrl)
-.then(response => {
-    return response.json()
-  })
-  .then(data => {
-    let gifData = data.data;
-    let gifUrls=[];
-    let trendGrid = document.getElementById("trendGrid");
- 
-    gifData.forEach(element => {
-      gifUrls.push(element.images.original.url)
-    });
-    console.log(gifUrls)
-    for (let i=0; i<gifUrls.length; i++){
-    trendGrid.innerHTML += '<img src="'+gifUrls[i]+' "class="gallery__img" />';
-    }
 
-  })
-  .then(
-1
-  )
+loadContent(`${mainURL}search?&api_key=${apiKey}&q=${keyword[0]}&limit=${limitSearch}&offset=0&rating=G&lang=eng`);
 
-  .catch(err => {
-    console.log("Fetch Error");
-  })
+document.getElementById("changePet").addEventListener("click", function(){
+  window.location = 'https://skydaitenshi.github.io/index.html';
+}); 
+
+document.getElementById("home").addEventListener("click", function(){
+  let pet = document.getElementById("zone").textContent.split(" ");
+  window.location = "https://skydaitenshi.github.io/animalPages/"+ pet[0].toLocaleLowerCase()+".html";
+}); 
+
+document.getElementById("searchInput").addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+
+    keyword[0] = document.getElementById("searchInput").value;
+    loadContent(`${mainURL}search?&api_key=${apiKey}&q=${keyword[0]}&limit=${limitSearch}&offset=0&rating=G&lang=eng`);
+    document.getElementById("searchInput").value="";
+  }
+});
+
+  function loadContent (getUrl) {
+    fetch(getUrl)
+    .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        let gifData = data.data;
+        let gifUrls=[];
+        let trendGrid = document.getElementById("trendGrid");
+        trendGrid.innerHTML="";
+     
+        gifData.forEach(element => {
+          gifUrls.push(element.images.original.url)
+        });
+        for (let i=0; i<gifUrls.length; i++){
+        trendGrid.innerHTML += '<img src="'+gifUrls[i]+' "class="gallery__img" />';
+        }
+    
+      })
+      .then(
+    
+      )
+    
+      .catch(err => {
+        console.log("Fetch Error");
+      })
+  }
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-
-// function catPage () {
-//     console.log('js works')
-
-// //   window.location = 'https://skydaitenshi.github.io/animalPages/cats.html';
-// }
-// document.getElementById("cats").addEventListener("click", catPage());
-
 document.getElementById("cats").addEventListener("click", function(){
-    // document.getElementById("demo").innerHTML = "Hello World";
-    console.log('js works')
     window.location = 'https://skydaitenshi.github.io/animalPages/cats.html';
+  }); 
+  document.getElementById("dogs").addEventListener("click", function(){
+    window.location = 'https://skydaitenshi.github.io/animalPages/dogs.html';
+  }); 
+  document.getElementById("sheep").addEventListener("click", function(){
+    window.location = 'https://skydaitenshi.github.io/animalPages/sheep.html';
+  }); 
+  document.getElementById("goats").addEventListener("click", function(){
+    window.location = 'https://skydaitenshi.github.io/animalPages/goats.html';
   }); 
